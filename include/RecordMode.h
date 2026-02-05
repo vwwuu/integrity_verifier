97d7039e049155aff670e7bdee95efb08ff69fd4
oddloops <vinciwu.vwu@gmail.com>
2026-02-04 23:37:11 -0500

    Added constructor and private attributes. Changed name and typing of parameter and method names.

--- include/RecordMode.h
+++ include/RecordMode.h
@@ -1,13 +1,19 @@
 #ifndef RECORDMODE_H
 #define RECORDMODE_H
 
+#include <filesystem>
+#include <stdexcept>
 #include <iostream>
 #include <string>
 
 class RecordMode
 {
- public:
-  bool verifyDirectory(const std::string& targetDirectory) const;
+public:
+  explicit RecordMode (const std::filesystem::path& targetDirectory);
+
+  private:
+  bool validateDirectoryPath(const std::filesystem::path& targetDirectory);
+  std::filesystem::path tDirectory;
 };
 
 #endif
