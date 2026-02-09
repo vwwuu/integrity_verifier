243b13e6c0d82ddda9a1555b04f9bb3fcc42fe9d
oddloops <vinciwu.vwu@gmail.com>
2026-02-08 20:38:03 -0500

    Updated validateDirectory method to use new struct return type

--- src/IntegrityCore.cc
+++ src/IntegrityCore.cc
@@ -1,13 +1,17 @@
 #include "../include/IntegrityCore.h"
 
-bool IntegrityCore::validateDirectory(const std::filesystem::path& targetDirectory)
+ValidateMessage IntegrityCore::validateDirectory(const std::filesystem::path& targetDirectory)
 {
   try {
     std::filesystem::file_status tDirectory = std::filesystem::status(targetDirectory);
-    return std::filesystem::is_directory(tDirectory);
+    if (!std::filesystem::is_directory(tDirectory)) {
+      return ValidateMessage{false, "Path not a directory"};
+    }
+    
+    return ValidateMessage{true, "Path is a directory"};
   }
-  catch (const std::runtime_error& e) {
+  catch (std::filesystem::filesystem_error const& e) {
     std::cout << "Filesystem error: " << e.what() << std::endl;
-    return false;
+    return ValidateMessage{false, e.what()};
   }
 }
