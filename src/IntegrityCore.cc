ce0a9ba411e0863c7399a9abbe48607323f43434
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-14 00:50:29 -0500

    Created function to set fileInfo struct and changed return type of getDirectoryContents

--- src/IntegrityCore.cc
+++ src/IntegrityCore.cc
@@ -1,6 +1,24 @@
 #include "../include/IntegrityCore.h"
+#include <map>
 
-bool IntegrityCore::validateDirectory(const std::filesystem::path& targetDirectory) const
+namespace {
+  std::string computeHash(std::filesystem::path const& filePath) {
+    return ""; // TODO
+  }
+
+  FileInfo setFileInfo(std::filesystem::directory_entry const& e) {
+    std::filesystem::path const ePath = e.path();
+    return FileInfo {
+      ePath.stem().string(),
+      ePath,
+      ePath.extension(),
+      computeHash(ePath),
+      std::chrono::system_clock::now()
+    };
+  }
+}
+
+bool IntegrityCore::validateDirectory(std::filesystem::path const& targetDirectory) const
 {
   try {
     std::filesystem::file_status tDirectory = std::filesystem::status(targetDirectory);
@@ -16,11 +34,19 @@ bool IntegrityCore::validateDirectory(const std::filesystem::path& targetDirecto
   }
 }
 
-std::map<std::filesystem::path, FileInfo> IntegrityCore::getDirectoryContents(const std::filesystem::path& targetDirectory) const
+std::map<std::filesystem::path, std::map<std::filesystem::path, FileInfo>> IntegrityCore::getDirectoryContents(std::filesystem::path const& targetDirectory) const
 {
   std::map<std::filesystem::path, FileInfo> directoryPaths;
   if (!this->validateDirectory(targetDirectory)) {
     return directoryPaths;
   }
+
+  for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(targetDirectory)) {
+    std::cout << dir_entry << std::endl;
+    auto const& fileData = std::filesystem::status(dir_entry);
+    if (std::filesystem::is_directory(fileData)) continue;
+  }
   return directoryPaths;
 }
+
+FileInfo setFile
