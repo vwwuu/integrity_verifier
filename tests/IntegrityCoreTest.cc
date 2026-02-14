a7f2b0e8d7ad41658a89ef3aa90520c6e0f797f3
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-14 18:57:08 -0500

    Removed lambda function and added test for createFileInfo

--- tests/IntegrityCoreTest.cc
+++ tests/IntegrityCoreTest.cc
@@ -42,7 +42,24 @@ TEST_F(IntegrityCoreTestClass, ValidateThrow) {
 			       std::filesystem::perm_options::add
 			      ); 
   std::filesystem::remove_all("sandbox");
-}	       
+}
+
+// createFileInfo tests
+TEST_F(IntegrityCoreTestClass, createFileInfo) {
+  const std::filesystem::path root = "sandbox";
+  const std::filesystem::path p1 = root/"c1";
+  std::filesystem::create_directories(p1);
+  TestHelpers::createFile(p1/"fileP1.txt", "Hello c1");
+  
+  FileInfo actualFileInfo = core.createFileInfo(p1);
+  FileInfo expectedFileInfo;
+  expectedFileInfo.fileName = "fileP1";
+  expectedFileInfo.filePath = p1;
+  expectedFileInfo.fileExtension = ".txt";
+  EXPECT_TRUE(TestHelpers::compareFileInfo(expectedFileInfo, actualFileInfo));
+
+  std::filesystem::remove_all("sandbox");
+}
 
 // getDirectoryContents Tests
 TEST_F(IntegrityCoreTestClass, getEmptyDirectory) {
@@ -50,9 +67,12 @@ TEST_F(IntegrityCoreTestClass, getEmptyDirectory) {
   std::filesystem::create_directories(p);
   std::map<std::filesystem::path, FileInfo> actualMap = core.getDirectoryContents(p);
   std::map<std::filesystem::path, FileInfo> expectedMap{};
+
   EXPECT_TRUE(TestHelpers::getDirectoryTestHelper(actualMap, expectedMap));
+
   std::filesystem::remove_all("sandbox");
 }
+
 
 TEST_F(IntegrityCoreTestClass, getDirectoryContentsOutput) {
   auto createFile = [](const std::filesystem::path& p, std::string_view pContents){
@@ -66,12 +86,12 @@ TEST_F(IntegrityCoreTestClass, getDirectoryContentsOutput) {
   std::filesystem::create_directories(p1);
   std::filesystem::create_directories(p2);
 
-  createFile(p1/"fileP1.txt", "Hello c1");
-  createFile(p1/"config.json", "{ \"key\": 1 }");
-  createFile(p1/"image.png", "png");
-  createFile(p1/"fileP2.txt", "Hello c2");
-  createFile(p2/"script.py", "print('Hello World!')");
-  createFile(p2/"archive.zip", "dummy zip");
+  TestHelpers::createFile(p1/"fileP1.txt", "Hello c1");
+  TestHelpers::createFile(p1/"config.json", "{ \"key\": 1 }");
+  TestHelpers::createFile(p1/"image.png", "png");
+  TestHelpers::createFile(p1/"fileP2.txt", "Hello c2");
+  TestHelpers::createFile(p2/"script.py", "print('Hello World!')");
+  TestHelpers::createFile(p2/"archive.zip", "dummy zip");
  
   std::map<std::filesystem::path, FileInfo> actualMap = core.getDirectoryContents(root);
   std::map<std::filesystem::path, FileInfo> expectedMap{
