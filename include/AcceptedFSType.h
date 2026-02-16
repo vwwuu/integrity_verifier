4cf9a98444f7f92ea9bbea85b2ce2317214ca7f3
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-16 16:13:30 -0500

    Renamed AcceptedFiles for clarity

---
+++ include/AcceptedFSType.h
@@ -0,0 +1,9 @@
+#ifndef ACCEPTED_FS_TYPE_H
+#define ACCEPTED_FS_TYPE_H
+
+enum class AcceptedFSType {
+  FILE,
+  DIRECTORY
+};
+
+#endif
--- include/AcceptedFiles.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACCEPTED_FILES_H
-#define ACCEPTED_FILES_H
-
-enum class AcceptedFile {
-  FILE,
-  DIRECTORY
-};
-
-#endif
