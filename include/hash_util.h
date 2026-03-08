0727d6b88e62bb284f35cea17a8cfe0c2a7fc357
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-03-08 13:34:23 -0400

    Removed redundant hash skeleton

--- include/hash_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef HASH_UTIL
-#define HASH_UTIL
-
-#include "openssl/sha.h"
-
-int sha256_file(char* path, char outputBuffer[65]);
-
-#endif
--- src/hash_util.cc
+++
@@ -1,5 +0,0 @@
-#include "../include/hash_util.h"
-
-int sha256_file(char* path, char outputBuffer[65]) {
-  return 0;
-}
