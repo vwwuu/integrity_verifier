6b8dbe033b56cb79cf84f440907ff902d60d940a
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-04-21 22:26:21 -0400

    Added implicit declaration for run and write mode methods

--- include/WriteMode.h
+++ include/WriteMode.h
@@ -1,15 +1,23 @@
-#ifndef WRITEMODE_H
+X #ifndef WRITEMODE_H
 #define WRITEMODE_H
 
 #include "IntegrityCore.h"
+    
+#include <filesystem>
+
+#include "DirectoryContent.h"
 
 class WriteMode
 {
 public:
   explicit WriteMode (IntegrityCore& core);
 
+  public:
+  bool run(std::filesystem::path const& directoryPath, std::filesystem::path const& outputPath); // calls writeRecord
+  
   private:
-  IntegrityCore& core;
+  IntegrityCore& _core;
+  bool writeRecord(DirectoryContent const& directoryContent, std::filesystem::path const& outputPath); // produce JSON file
 };
 
 #endif
