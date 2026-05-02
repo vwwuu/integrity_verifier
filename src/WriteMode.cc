5da869e92e88bbc8d651ea6caf74cdea05bcd425
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-05-01 22:55:45 -0400

    Added validation for input file paths

--- src/WriteMode.cc
+++ src/WriteMode.cc
@@ -1,5 +1,22 @@
 #include "WriteMode.h"
+#include "AcceptedFSType.h"
 
 bool WriteMode::run(std::filesystem::path const& directoryPath,
 		    std::filesystem::path const& outputPath) {
+  // validate ouput path
+  if (!_core.validatePath(outputPath, AcceptedFSType::DIRECTORY)) {
+    return false;
+  }
+  
+  // validate input path
+  if (!_core.validatePath(directoryPath, AcceptedFSType::DIRECTORY)) {
+    return false;
+  }
+
+  return false;
+}
+
+bool WriteMode::writeRecord(DirectoryContent const& directoryContent,
+			    std::filesystem::path const& outputPath) {
+  return false;
 }
