66e84bc7cf59d7bde4da4a7c83aa391b7cc593b7
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-05-01 22:56:12 -0400

    Defined test for successful paths

--- tests/WriteModeTest.cc
+++ tests/WriteModeTest.cc
@@ -10,6 +10,12 @@ protected:
   WriteMode Writer{Core};
 };
 
-TEST_F(WriteModeTestClass, ValidateWriteMode) {
-  EXPECT_TRUE(false);
+TEST_F(WriteModeTestClass, ValidateWriteModePaths) {
+    const std::filesystem::path inputDir  = std::filesystem::temp_directory_path() / "ValidateInput";
+    const std::filesystem::path outputDir = std::filesystem::temp_directory_path() / "ValidateOutput";
+
+    std::filesystem::create_directory(inputDir);
+    std::filesystem::create_directory(outputDir);
+
+    EXPECT_TRUE(Writer.run(inputDir, outputDir));
 }
